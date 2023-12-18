ea6a2969b1157a32d200ac858c853a6e0734d18d
Ellie22302 <nopeprobnot@gmail.com>
2023-12-18 11:19:48 -0500

    Added the prompts as alerts

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ var numbersArray=['1','2','3','4','5','6','7','8','9','0'];
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", writePassword);
+
+//Prompts for the password
+function getPrompts(){
+    choiceArray = [];
+
+    characterLength= parseInt(prompt("How many characters do you want in your secure password 8-128?"))
+
+    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128){
+        alert("Character has to be a number and within 8 - 128");
+        return false;
+    }
+    if (confirm("Would you like Lowercase?")){
+        choiceArray = choiceArray.concat(lowerCase);
+    }
+    if (confirm("Would you like Uppercase?")){
+        choiceArray = choiceArray.concat(upperCase);
+    }
+    if (confirm("Would you like Special Characters?")){
+        choiceArray = choiceArray.concat(specialChar);
+    }
+    if (confirm("Would you like Numbers?")){
+        choiceArray = choiceArray.concat(numbersArray);
+    }
+    return true;
+}
